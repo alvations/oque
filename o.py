8057bf308df692ae7b016db9de753f1da9c80bfe
alvations <alvations@gmail.com>
2015-06-01 16:22:03 +0200

    okay

---
+++ o.py
@@ -0,0 +1,74 @@
+
+import io
+import numpy as np
+
+from gensim.utils import tokenize
+from gensim.models import Word2Vec, Phrases
+
+def sent_vectorizer(sent, model):
+    sent_vec = np.zeros(400)
+    numw = 0
+    for w in sent:
+        try:
+            sent_vec = np.add(sent_vec, model[w])
+            numw+=1
+        except:
+            pass
+    return sent_vec/numw
+
+def cosine(s1, s2):
+    cos_score = np.dot(s1, s2)
+    if cos_score == np.NAN:
+        return 0
+    else:
+        return cos_score
+    
+def load_neural_model(lang, model_path='neural-model/'):
+    model = Word2Vec.load(model_path+'wmt-metrics.'+lang+'.w2v')
+    bigram_transformer = Phrases(model_path+'bigram_transformer.'+lang+'.pk')
+    return bigram_transformer, model
+
+def create_cosine_feature(direction, dataset, quest_data_path='quest/'):
+    ''' 
+    # USAGE:
+    direction = 'en-de'
+    dataset = 'test'
+    for i in create_cosine_feature(direction, dataset):
+        print i
+    direction = 'en-de'
+    dataset = 'training'
+    for i in create_cosine_feature(direction, dataset):
+        print i
+    '''
+    # Load neural models for src and target.
+    srclang, trglang = direction.lower().split('-')
+    src_bigram_transformer, src_model = load_neural_model(srclang)
+    trg_bigram_transformer, trg_model = load_neural_model(trglang)
+    
+    # Loads quest training files
+    srcfile = quest_data_path+direction+'_source.' + dataset
+    trgfile = quest_data_path+direction+'_target.' + dataset
+
+    with io.open(srcfile, 'r') as srcfin,  \
+    io.open(trgfile, 'r') as trgfin:
+        # Phrasalize the sentences.
+        src_sents = src_bigram_transformer[[tokenize(i.strip()) for i in srcfin]]
+        trg_sents = trg_bigram_transformer[[tokenize(i.strip()) for i in trgfin]]
+        for srcline, trgline in zip(src_sents, trg_sents):
+            # Tokenizes the characters list of list... 
+            # TODO: merge this with the phrasalized step
+            src_sent = ["".join(i) for i in list(srcline)] 
+            trg_sent = ["".join(i) for i in list(trgline)]
+            # Create sentence vectors
+            src_vector = sent_vectorizer(src_sent, src_model)
+            trg_vector = sent_vectorizer(trg_sent, trg_model)
+            # Extract cosines 
+            yield cosine(src_vector, trg_vector)
+
+def cosine_feature(direction, dataset):
+    x = np.array(list(create_cosine_feature(direction, dataset)))
+    where_are_NaNs = np.isnan(x)
+    x[where_are_NaNs] = 0
+    return x.reshape(len(x), 1)
+            
+    
---
+++ que.py
@@ -0,0 +1,86 @@
+
+import io
+
+import numpy as np
+from scipy.stats import uniform as sp_rand
+
+from sklearn.linear_model import BayesianRidge
+from sklearn.grid_search import RandomizedSearchCV
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+from o import cosine_feature
+
+def load_quest(direction, dataset, which_data, quest_data_path='quest/'):
+    '''
+    # USAGE:
+    baseline_train = load_quest('en-de', 'training', 'baseline17')
+    baseline_train = load_quest('en-de', 'test', 'baseline17')
+    meteor_train = load_quest('en-de', 'training', 'meteor')
+    meteor_test =  load_quest('en-de', 'test', 'meteor')
+    '''
+    return np.loadtxt(quest_data_path+direction+'.'+dataset+'.'+which_data)
+
+    
+def experiments(direction, to_tune, with_cosine, classifier=None):
+    '''
+    # USAGE:
+    direction = 'en-de'
+    to_tune = False
+    with_cosine = False
+    outfilename, mae, mse = experiments(direction, to_tune, with_cosine)
+    print outfilename, mae, mse
+    '''
+    train, test = 'training', 'test'
+    # Load train data
+    baseline_train = load_quest(direction, train, 'baseline17')
+    cos_train = cosine_feature(direction, train)
+    meteor_train = load_quest(direction, train, 'meteor')
+    # Load test data
+    baseline_test = load_quest(direction, test, 'baseline17')
+    cos_test = cosine_feature(direction, test)
+    meteor_test = load_quest(direction, test, 'meteor')
+    
+    # Create training array and outputs
+    X_train = baseline_train
+    if with_cosine:
+        X_train = np.concatenate((baseline_train, cos_train), axis=1)
+    y_train = meteor_train
+    # Create testing array and gold answers
+    X_test = baseline_test
+    if with_cosine:
+        X_test = np.concatenate((baseline_test, cos_test), axis=1)
+    y_test = meteor_test
+    
+    
+    # Initialize Classifier.
+    clf = BayesianRidge()
+    if classifier:
+        clf = classifier
+        to_tune = False
+    if to_tune:
+        # Grid search: find optimal classifier parameters.
+        param_grid = {'alpha_1': sp_rand(), 'alpha_2': sp_rand()}
+        rsearch = RandomizedSearchCV(estimator=clf, 
+                                     param_distributions=param_grid, n_iter=5000)
+        rsearch.fit(X_train, y_train)
+        # Use tuned classifier.
+        clf = rsearch.best_estimator_
+          
+    # Trains Classifier   
+    clf.fit(X_train, y_train)
+    
+    # Outputs to file.
+    to_tune_str = 'tuned' if to_tune else 'notune'
+    model_name = 'withcosine' if with_cosine else 'baseline'
+    outfile_name = ".".join(['oque',model_name,to_tune_str,direction,'output']) 
+    with io.open(outfile_name, 'w') as fout:
+        answers = []
+        for i in clf.predict(X_test):
+            answers.append(i)
+            fout.write(unicode(i)+'\n')
+    
+    mse = mean_squared_error(y_test, np.array(answers))
+    mae = mean_absolute_error(y_test, np.array(answers))
+    return outfile_name, mae, mse
+
+
