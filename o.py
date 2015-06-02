c787f71362b4589dc04e2af3c4f704f8e551a25f
alvations <alvations@gmail.com>
2015-06-02 17:05:13 +0200

    made tonnes of changes

--- o.py
+++ o.py
@@ -8,11 +8,14 @@ from gensim.models import Word2Vec, Phrases
 #from nltk.corpus import stopwords
 #eng_stops = stopwords.words('english')
 
+vocab = [i.strip() for i in io.open('test-vocab.txt')]
 
 def sent_vectorizer(sent, model):
     sent_vec = np.zeros(400)
     numw = 0
     for w in sent:
+        if w not in vocab:
+            continue
         try:
             sent_vec = np.add(sent_vec, model[w])
             numw+=1
@@ -23,7 +26,7 @@ def sent_vectorizer(sent, model):
 def cosine(s1, s2):
     cos_score = np.dot(s1, s2)
     if cos_score == np.NAN:
-        return 0
+        return 9.9999e-11
     else:
         return cos_score
     
@@ -31,6 +34,21 @@ def load_neural_model(lang, model_path='neural-model/'):
     model = Word2Vec.load(model_path+'wmt-metrics.'+lang+'.w2v')
     bigram_transformer = Phrases(model_path+'bigram_transformer.'+lang+'.pk')
     return bigram_transformer, model
+
+def src_trg_sent_generator(direction, dataset, quest_data_path,
+                           src_bigram_transformer, trg_bigram_transformer):
+    # Loads quest training files
+    srcfile = quest_data_path+direction+'_source.' + dataset
+    trgfile = quest_data_path+direction+'_target.' + dataset
+
+    with io.open(srcfile, 'r') as srcfin,  \
+    io.open(trgfile, 'r') as trgfin:
+        # Phrasalize the sentences.
+        src_sents = src_bigram_transformer[[tokenize(i.strip()) for i in srcfin]]
+        trg_sents = trg_bigram_transformer[[tokenize(i.strip()) for i in trgfin]]
+        for srcline, trgline in zip(src_sents, trg_sents):
+            yield srcline, trgline
+
 
 def create_cosine_feature(direction, dataset, quest_data_path='quest/'):
     ''' 
@@ -49,25 +67,20 @@ def create_cosine_feature(direction, dataset, quest_data_path='quest/'):
     src_bigram_transformer, src_model = load_neural_model(srclang)
     trg_bigram_transformer, trg_model = load_neural_model(trglang)
     
-    # Loads quest training files
-    srcfile = quest_data_path+direction+'_source.' + dataset
-    trgfile = quest_data_path+direction+'_target.' + dataset
-
-    with io.open(srcfile, 'r') as srcfin,  \
-    io.open(trgfile, 'r') as trgfin:
-        # Phrasalize the sentences.
-        src_sents = src_bigram_transformer[[tokenize(i.strip()) for i in srcfin]]
-        trg_sents = trg_bigram_transformer[[tokenize(i.strip()) for i in trgfin]]
-        for srcline, trgline in zip(src_sents, trg_sents):
-            # Tokenizes the characters list of list... 
-            # TODO: merge this with the phrasalized step
-            src_sent = ["".join(i) for i in list(srcline)] 
-            trg_sent = ["".join(i) for i in list(trgline)]
-            # Create sentence vectors
-            src_vector = sent_vectorizer(src_sent, src_model)
-            trg_vector = sent_vectorizer(trg_sent, trg_model)
-            # Extract cosines 
-            yield cosine(src_vector, trg_vector)
+    sent_generator = src_trg_sent_generator(direction, dataset, quest_data_path,
+                                            src_bigram_transformer,
+                                            trg_bigram_transformer)
+    for srcline, trgline in sent_generator:
+        # Tokenizes the characters list of list... 
+        # TODO: merge this with the phrasalized step
+        src_sent = ["".join(i) for i in list(srcline)] 
+        trg_sent = ["".join(i) for i in list(trgline)]
+        # Create sentence vectors
+        src_vector = sent_vectorizer(src_sent, src_model)
+        trg_vector = sent_vectorizer(trg_sent, trg_model)
+        # Extract cosines 
+        yield cosine(src_vector, trg_vector)
+    
 
 def cosine_feature(direction, dataset):
     x = np.array(list(create_cosine_feature(direction, dataset)))
@@ -75,4 +88,28 @@ def cosine_feature(direction, dataset):
     x[where_are_NaNs] = 9.9999e-11
     return x.reshape(len(x), 1)
             
+def create_complexity_feature(direction, dataset, quest_data_path='quest/'):
+    # Load neural models for src and target.
+    srclang, trglang = direction.lower().split('-')
+    src_bigram_transformer, src_model = load_neural_model(srclang)
+    trg_bigram_transformer, trg_model = load_neural_model(trglang)
     
+    sent_generator = src_trg_sent_generator(direction, dataset, quest_data_path,
+                                            src_bigram_transformer,
+                                            trg_bigram_transformer)
+    for srcline, trgline in sent_generator:
+        # Tokenizes the characters list of list... 
+        # TODO: merge this with the phrasalized step
+        src_sent = ["".join(i) for i in list(srcline)] 
+        trg_sent = ["".join(i) for i in list(trgline)]
+        # Create sentence vectors
+        src_vector = sent_vectorizer(src_sent, src_model)
+        trg_vector = sent_vectorizer(trg_sent, trg_model)
+        yield src_vector, trg_vector
+
+def complexity_feature(direction, dataset):
+    x1, x2 = zip(*list(create_complexity_feature(direction, dataset)))
+    x = np.concatenate((x1, x2), axis=1)
+    where_are_NaNs = np.isnan(x)
+    x[where_are_NaNs] = 9.9999e-11
+    return x
--- que.py
+++ que.py
@@ -3,15 +3,20 @@ import io
 
 import numpy as np
 from scipy.stats import uniform as sp_rand
+from itertools import combinations
 
 from sklearn.linear_model import BayesianRidge
 from sklearn.grid_search import RandomizedSearchCV
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
 
-from o import cosine_feature
+from o import cosine_feature, complexity_feature
 
-def load_quest(direction, dataset, which_data, quest_data_path='quest/'):
+train, test = 'training', 'test'
+
+def load_quest(direction, dataset, which_data, quest_data_path='quest/', 
+               to_normalize=True):
     '''
     # USAGE:
     baseline_train = load_quest('en-de', 'training', 'baseline17')
@@ -20,51 +25,39 @@ def load_quest(direction, dataset, which_data, quest_data_path='quest/'):
     meteor_test =  load_quest('en-de', 'test', 'meteor')
     '''
     x = np.loadtxt(quest_data_path+direction+'.'+dataset+'.'+which_data)
-    x = x / np.linalg.norm(x)
+    if to_normalize:
+        x = x / np.linalg.norm(x)
     return x
 
-    
-def experiments(direction, to_tune, with_cosine, classifier=None):
-    '''
-    # USAGE:
-    direction = 'en-de'
-    to_tune = False
-    with_cosine = False
-    outfilename, mae, mse = experiments(direction, to_tune, with_cosine)
-    print outfilename, mae, mse
-    '''
-    train, test = 'training', 'test'
+def load_wmt15_data(direction):    
     # Load train data
-    baseline_train = load_quest(direction, train, 'baseline17') 
-    cos_train = cosine_feature(direction, train)
-    meteor_train = load_quest(direction, train, 'meteor')
+    baseline_train = load_quest(direction, train, 'baseline17', to_normalize=False)     
+    meteor_train = load_quest(direction, train, 'meteor', to_normalize=False)
     # Load test data
-    baseline_test = load_quest(direction, test, 'baseline17')
+    baseline_test = load_quest(direction, test, 'baseline17', to_normalize=False)
+    meteor_test = load_quest(direction, test, 'meteor', to_normalize=False)
+    return baseline_train, meteor_train, baseline_test, meteor_test
+    
+
+def load_cosine_features(direction):
+    cos_train = cosine_feature(direction, train)
+    #cos_train = complexity_feature(direction, train)
     cos_test = cosine_feature(direction, test)
-    meteor_test = load_quest(direction, test, 'meteor')
-    
-    # Create training array and outputs
-    X_train = baseline_train
-    if with_cosine:
-        X_train = np.concatenate((baseline_train, cos_train), axis=1)
-    y_train = meteor_train
-    # Create testing array and gold answers
-    X_test = baseline_test
-    if with_cosine:
-        X_test = np.concatenate((baseline_test, cos_test), axis=1)
-    y_test = meteor_test
-    
-    
+    #cos_test = complexity_feature(direction, test)
+    return cos_train, cos_test    
+
+def train_classiifer(X_train, y_train, to_tune, classifier):
     # Initialize Classifier.
     clf = BayesianRidge()
-    #clf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    clf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    #clf = RandomForestRegressor()
     if classifier:
         clf = classifier
         to_tune = False
     if to_tune:
         # Grid search: find optimal classifier parameters.
         param_grid = {'alpha_1': sp_rand(), 'alpha_2': sp_rand()}
-        #param_grid = {'C': sp_rand(), 'gamma': sp_rand()}
+        param_grid = {'C': sp_rand(), 'gamma': sp_rand()}
         rsearch = RandomizedSearchCV(estimator=clf, 
                                      param_distributions=param_grid, n_iter=5000)
         rsearch.fit(X_train, y_train)
@@ -73,31 +66,126 @@ def experiments(direction, to_tune, with_cosine, classifier=None):
           
     # Trains Classifier   
     clf.fit(X_train, y_train)
+    return clf
+
+
+def brute_force_feature_selection():
+    x = range(1,18)
+    for l in range (1,len(x)+1):
+        for f in list(combinations(range(0,len(x)),l)):
+            yield f
+
+def evaluate_classifier(clf, X_test, direction, with_cosine, 
+                        to_tune, to_output=True, to_hack=False):
+    answers = list(clf.predict(X_test))
+    if to_hack:
+        hacked_answers = []
+        for i,j in zip(answers, X_test):
+            if j[9] > 0.7 and j[0] < 12: i = i - 0.2;
+            if j[0] ==1 or j[1]== 1: i = i - 0.15;
+            if j[0] > 200: i = i - 0.1;
+            if i < 0: i = 0.0;
+            hacked_answers.append(i)
+        answers = hacked_answers
+    outfile_name = ''
+    if to_output: # Outputs to file.
+        to_tune_str = 'tuned' if to_tune else 'notune'
+        model_name = 'withcosine' if with_cosine else 'baseline'
+        outfile_name = ".".join(['oque',model_name,
+                                 to_tune_str,direction,'output'])
+        with io.open(outfile_name, 'w') as fout:
+            for i in answers:
+                fout.write(unicode(i)+'\n')
+    return answers, outfile_name
+                
     
-    # Outputs to file.
-    to_tune_str = 'tuned' if to_tune else 'notune'
-    model_name = 'withcosine' if with_cosine else 'baseline'
-    outfile_name = ".".join(['oque',model_name,to_tune_str,direction,'output']) 
-    with io.open(outfile_name, 'w') as fout:
-        answers = []
-        for i in clf.predict(X_test):
-            answers.append(i)
-            fout.write(unicode(i)+'\n')
+def experiments(direction, with_cosine, to_tune, to_output=True, to_hack=False, 
+                to_debug=False, classifier=None):
+    '''
+    # USAGE:
+    direction = 'en-de'
+    to_tune = False
+    with_cosine = False
+    outfilename, mae, mse = experiments(direction, to_tune, with_cosine)
+    print outfilename, mae, mse
+    '''
+    # Create training and testing array and outputs
+    X_train, y_train, X_test, y_test = load_wmt15_data(direction)
+    if with_cosine:
+        # Create cosine features for training
+        cos_train, cos_test = load_cosine_features(direction)
+        X_train = np.concatenate((X_train, cos_train), axis=1)
+        X_test = np.concatenate((X_test, cos_test), axis=1)
     
-    mse = mean_squared_error(y_test, np.array(answers))
-    mae = mean_absolute_error(y_test, np.array(answers))
+    for f in brute_force_feature_selection():
+        _X_train = X_train[:, f]
+        
+        _X_test = X_test[:, f]
+        # Train classifier
+        clf = train_classiifer(_X_train, y_train, to_tune, classifier)
+        answers, outfile_name = evaluate_classifier(clf, _X_test, direction, 
+                                               with_cosine, to_tune, 
+                                               to_output=False, to_hack=False)
     
-    #for i,j,k in zip(answers, y_test, baseline_train):
-    #    print i,j, k
+        mse = mean_squared_error(y_test, np.array(answers))
+        mae = mean_absolute_error(y_test, np.array(answers))
+        print mae, f
     
+    if to_debug:
+        srcfile = io.open('quest/en-de_source.test', 'r')
+        trgfile = io.open('quest/en-de_target.test', 'r')
+        cos_train, cos_test = load_cosine_features(direction)
+        for i,j,k,s,t, c in zip(answers, y_test, X_test, 
+                                srcfile, trgfile, cos_test):   
+            if i - j > 0.095 or j -1 > 0.095 or c == 9.99990000e-11: 
+                print i, j, k[0], k[9], k, c
+                print s, t
+        
     return outfile_name, mae, mse
 
-
-# 0.0967
-
 direction = 'en-de'
-to_tune = False
 with_cosine = False
-outfilename, mae, mse = experiments(direction, to_tune, with_cosine)
+to_tune = False
+to_output = True
+outfilename, mae, mse = experiments(direction, with_cosine,to_tune, to_output, to_debug=False)
 print outfilename, mae, mse
 
+# DE-EN
+# no-hack at all
+# oque.baseline.notune.de-en.output 0.0692666454858 0.011038250617
+# no-hack, with cosine
+# oque.withcosine.notune.de-en.output 0.0692590476386 0.0110349222335
+
+# Super default + hack
+# oque.baseline.notune.de-en.output 0.0685437539196 0.0106677292505
+# hacked
+# if j[0] ==1 or j[1]== 1: i = i - 0.15
+# oque.withcosine.notune.de-en.output 0.0685361560723 0.0106643693054
+
+
+
+# EN-DE
+# oque.baseline.notune.en-de.output 0.0980804849285 0.0184924281565
+
+# if j[9] > 0.7 and j[0] < 12: i = i -0.2
+# oque.baseline.notune.en-de.output 0.097544087243 0.0208756823852 
+# oque.withcosine.notune.en-de.output 0.0975427119756 0.0208755274686
+
+# if j[9] > 0.7 and j[0] < 12: i = i -0.2
+# if j[0] ==1 or j[1]== 1: i = i - 0.1
+# oque.withcosine.notune.en-de.output 0.0973017481202 0.0207602928984
+
+# if j[9] > 0.7 and j[0] < 12: i = i -0.2
+# if j[0] ==1 or j[1]== 1: i = i - 0.15
+# oque.withcosine.notune.en-de.output 0.0972310140807 0.0207568924808
+
+# if j[9] > 0.7 and j[0] < 12: i = i -0.2
+# if j[0] ==1 or j[1]== 1: i = i - 0.15
+# if j[0] > 200: i = i - 0.1
+# oque.withcosine.notune.en-de.output 0.0968903228194 0.0206775825255
+
+# if j[9] > 0.7 and j[0] < 12: i = i -0.2
+# if j[0] ==1 or j[1]== 1: i = i - 0.15
+# if j[0] > 200: i = i - 0.1
+# if i < 0: i = 0.0
+# oque.withcosine.notune.en-de.output 0.0968359771138 0.0206633629455
